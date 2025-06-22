4d837430c668c1d37c2cd3467e8e50945545f131
Sara Polselli <paulsally@gmail.com>
2025-06-22 03:37:03 +0200

    Third exercise L00-00-print_numbers. Separate file main.c  to test compilation

---
+++ my-attempts/Level 00/0-0-ft_print_numbers/ft_print_numbers.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/13 21:38:04 by fwuensch          #+#    #+#             */
+/*   Updated: 2017/07/13 21:41:27 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	int		number;
+	char	digit;
+
+	number = 0;
+	while (number <= 9)
+	{
+		digit = '0' + number;
+		write (1, &digit, 1);
+		number++;
+	}
+	write (1, "\n", 1);
+}
